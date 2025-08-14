46ea9c8b1fcdf6d0d5f420ba124e6a74fc7fbd22
Davlatov Oybek <davlatovoybek007@gmail.com>
2025-08-14 08:32:43 +0500

    Button added, working now

--- src/main/java/com/example/projectcab202/HelloController.java
+++ src/main/java/com/example/projectcab202/HelloController.java
@@ -2,13 +2,17 @@ package com.example.projectcab202;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label welcomeLabel;
+
+    @FXML
+    private TextField nameInput;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeLabel.setText("Hello, " + nameInput.getText() + "!");
     }
 }

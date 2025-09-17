da6d51e37b7cb0e60973e48ac9088a8cf097a26f
Fynn_lab <fynnoshea9090@gmail.com>
2025-09-18 06:49:01 +1000

    Create quiz UI now interacts with DB, need to add on back button functionality

--- src/main/java/com/example/projectcab302/Controller/CreateQuizController.java
+++ src/main/java/com/example/projectcab302/Controller/CreateQuizController.java
@@ -1,4 +1,56 @@
 package com.example.projectcab302.Controller;
 
+import com.example.projectcab302.Model.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class CreateQuizController {
+    @FXML
+    private TextField questionField;
+    @FXML
+    private TextField optionAField;
+    @FXML
+    private TextField optionBField;
+    @FXML
+    private TextField optionCField;
+    @FXML
+    private TextField optionDField;
+    @FXML
+    private TextField answerField;
+
+    @FXML
+    private IQuizDAO quizDAO;
+
+    @FXML
+    protected void onsubmitQuiz() throws IOException {
+        quizDAO = new SqlQuizDAO();
+        quizDAO.clearData();
+
+        if (questionField.isEmpty() || optionAField.isEmpty() || optionBField.isEmpty() || optionCField.isEmpty() || optionDField.isEmpty() || answerField.isEmpty()) {
+            errorLabel.setText("Please fill in all fields.");
+            return;
+        }
+
+        String question = questionField.getText();
+        String optionA = optionAField.getText();
+        String optionB = optionBField.getText();
+        String optionC = optionCField.getText();
+        String optionD = optionDField.getText();
+        String answer = answerField.getText();
+        String course = "CAB302";
+
+        Quiz quizs = new Quiz(question, optionA, optionB, optionC, optionD, answer, course);
+        quizDAO.addQuiz(quizs);
+
+
+    }
 }
